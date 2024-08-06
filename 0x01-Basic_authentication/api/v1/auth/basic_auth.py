ddbb90beb3c150bde171269b8884d8841f99404d
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-07 00:38:16 +0300

    check 8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,7 +10,7 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """ eturns the Base64 part of the Authorization header
+        """ Returns the Base64 part of the Authorization header
         for a Basic Authentication
         """
         if (authorization_header is None
@@ -22,6 +22,8 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
             self,
             base64_authorization_header: str) -> str:
+        """ Returns the decoded value of a Base64 string
+        """
         if (base64_authorization_header is None
                 or type(base64_authorization_header) is not str):
             return None
