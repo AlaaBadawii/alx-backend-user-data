77f52e9198c327563c73d26147de3975e2e48a26
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-06 20:29:04 +0300

    check 3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Auth class to manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ for authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
