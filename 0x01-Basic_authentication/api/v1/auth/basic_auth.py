9cadf3b1575fe65cfaf369989f3ad06d400a7070
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-07 00:57:43 +0300

    check 9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,16 @@ class BasicAuth(Auth):
             return res
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
+        """returns the user email and password from the Base64 decoded value.
+        """
+        if (decoded_base64_authorization_header is None
+                or type(decoded_base64_authorization_header) is not str
+                or ':' not in decoded_base64_authorization_header):
+            return (None, None)
+
+        (v1, v2) = decoded_base64_authorization_header.split(':')
+        return (v1, v2)
