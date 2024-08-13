248f8fc5877f8851eeef5099b731ec254288feca
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-13 20:26:12 +0300

    check 0. User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ user moduel """
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Integer, String, Column
 
 Base = declarative_base()
