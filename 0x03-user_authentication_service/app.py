7729a0e8a30372c6ae78e2581ea07e61140b8d7c
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-14 01:37:57 +0300

    check

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,13 +2,29 @@
 """ app module
 """
 from flask import Flask, render_template, request, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def message():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    auth_instance = Auth()
+
+    try:
+        user = auth_instance.register_user(email, password)
+        return jsonify({"email": f"{user.email}", "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
