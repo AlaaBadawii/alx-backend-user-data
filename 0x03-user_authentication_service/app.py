a31ba50109f4215dfef5fcc690fbf0879977d00f
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-14 16:23:57 +0300

    check 11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -27,20 +27,20 @@ def register():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """ create a new session for the user,
     store it the session ID as a cookie with key
     """
-    email = request.form.get('email')
-    pwd = request.form.get('password')
+    email, password = request.form.get("email"), request.form.get("password")
 
-    if not Auth.valid_login(email=email, password=pwd):
+    if not Auth.valid_login(email, password):
         abort(401)
 
-    session_id = Auth.create_session(email=email)
+    session_id = Auth.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
+
     return response
 
 
