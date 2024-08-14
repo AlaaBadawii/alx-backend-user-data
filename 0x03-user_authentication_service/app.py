10b68c7ecc3a48de1f6e82b3bc3add75e00166b7
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-14 16:25:54 +0300

    check 11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,6 +26,7 @@ def register():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
     """POST /sessions
