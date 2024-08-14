d34ce44743f5df04f1691cf512dcbb7c7d020b91
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-08-14 21:47:11 +0300

    check 19. Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,8 +78,24 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-
-
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """ Reset user's password
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        current_token = AUTH.get_reset_password_token(email)
+    except Exception:
+        abort(403)
+    if current_token == reset_token:
+        try:
+            AUTH.update_password(reset_token, new_password)
+        except Exception:
+            abort(403)
+        return jsonify({"email": "<user email>", "message": "Password updated"}), 200
+    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000"), 200
